306c2b9a57bcca8e78192cf6218f4b4e5a3ae366
Luis Henrique da Silva Carvalho <luisdev9225@gmail.com>
2022-10-20 23:58:36 -0300

    :bricks: ci: Implementação da autenticação do usuário

--- luis-blog/src/App.jsx
+++ luis-blog/src/App.jsx
@@ -1,29 +1,73 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth"; //mapeia de auth foi feita com sucesso
 
-import './App.css';
-import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom'
+//hooks
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
 
 //  PAGES
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
-
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Dashboard from "./pages/Dashboard/Dashboard";
 // COMPONENTS
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
+// Context
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      console.log(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
-      <Navbar/>
-        <div className='container'>
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/about' element={<About/>} />
-          </Routes>
-        </div>
-      <Footer/>
-      </BrowserRouter>
+      <AuthProvider value={{ user }}>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to={"/"} />}
+              />
+              <Route
+                path="/register"
+                element={!user ? <Register /> : <Navigate to={"/"} />}
+              />
+              <Route
+                path="/posts/create"
+                element={user ? <CreatePost /> : <Navigate to={"/login"} />}
+              />
+              <Route
+                path="/dashboard"
+                element={user ? <Dashboard /> : <Navigate to={"/login"} />}
+              />
+              
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
---
+++ luis-blog/src/pages/Register/Register.jsx
@@ -0,0 +1,108 @@
+import styles from "./Register.module.css";
+
+import { useState, useEffect } from "react";
+import { useAuthentication } from "../../hooks/useAuthentication";
+
+const Register = () => {
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const { createUser, error: authError, loading } = useAuthentication()
+
+  const handleSubmit  = async (e) => {
+    e.preventDefault();
+
+    setError("");
+
+    const user = {
+      displayName,
+      email,
+      password,
+    };
+
+    if (password !== confirmPassword) {
+      setError("As senhas precisam ser iguais!");
+      console.log(error);
+      return;
+    }
+
+    const res = await createUser(user)
+
+    console.log(user);
+
+    setEmail("");
+    setDisplayName("");
+    setPassword("");
+    setconfirmPassword("");
+  }; 
+
+  useEffect(() => {
+      setError(authError)
+  }, [authError])
+
+  return (
+    <div className={styles.register}>
+      <h1>Cadastre-se para postar</h1>
+      <p>Crie seu usuário e Compartilhe suas Histórias</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Nome:</span>
+          <input
+            type="text"
+            name="displayName"
+            onSubmit={handleSubmit}
+            required
+            placeholder="Nome do usuário"
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="E-mail do usuário"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Insira sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <span>Confime Senha:</span>
+          <input
+            type="password"
+            name="confirmPassword"
+            required
+            placeholder="Confirme a sua senha"
+            value={confirmPassword}
+            onChange={(e) => setconfirmPassword(e.target.value)}
+          />
+        </label>
+        {!loading && <button className="btn">Cadastrar</button>}
+        {loading && <button className="btn">Aguarde...</button>}
+        
+        {error && <p className="error"> {error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default Register;
