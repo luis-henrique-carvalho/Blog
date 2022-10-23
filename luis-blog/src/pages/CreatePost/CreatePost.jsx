48a4676c06a06f3873bd3e56da150797f21314ac
Luis Henrique da Silva Carvalho <luisdev9225@gmail.com>
2022-10-23 17:21:24 -0300

    :sparkles: feat: Integração com o fire base e criação dos metodos fetch e insertDocument

--- luis-blog/src/pages/CreatePost/CreatePost.jsx
+++ luis-blog/src/pages/CreatePost/CreatePost.jsx
@@ -1,10 +1,117 @@
-import React from 'react'
-import styles from './CreatePost.module.css'
+import React from "react";
+import styles from "./CreatePost.module.css";
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
 
 const CreatePost = () => {
-  return (
-    <div><h1>CreatePost</h1></div>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
 
-export default CreatePost
+  const { user } = useAuthValue();
+
+  const { insertDocument, response } = useInsertDocument("posts");
+
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError("");
+    
+
+    //validar imagem url
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
+    }
+
+    //criar o array de tags
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase())
+
+    //chegar todos os valores
+    if(!title || !image || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos")
+    }
+    if (formError) return
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tags: tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+
+    //redirect from home page
+
+    navigate("/")
+  };
+  return (
+    <div className={styles.create_post}>
+      <h2>Criar Post</h2>
+      <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Titulo:</span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Pense em um bom título"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+
+        <label>
+          <span>Imagem:</span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="Insira uma imagem"
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+          />
+        </label>
+
+        <label>
+          <span>Conteudo:</span>
+          <textarea
+            name="body"
+            required
+            placeholder="Insira o conteudo do post"
+            onChange={(e) => setBody(e.target.value)}
+            value={body}
+          ></textarea>
+        </label>
+
+        <label>
+          <span>Tags:</span>
+          <input
+            type="text"
+            name="tags"
+            required
+            placeholder="Insira as tags separadas por virgula"
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+          />
+        </label>
+        {!response.loading && <button className="btn">Criar post</button>}
+        {response.loading && <button className="btn">Aguarde...</button>}
+        {response.error && <p className="error"> {response.error}</p>}
+        {formError && <p className="error"> {formError}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
--- luis-blog/src/pages/Home/Home.jsx
+++ luis-blog/src/pages/Home/Home.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 // CSS
-import styles from './Home.module.css'
+import styles from "./Home.module.css";
 
 const Home = () => {
+  const { documents: posts, loading } = useFetchDocuments("posts");
+  const [query, setQuery] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+
+      <div className={styles.noposts}>
+      {loading && <p>Carregando...</p>}
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+        {posts && posts.map((post) => <h2>{post.title}</h2>)}
+      </div>
     </div>
   );
 };
